ca103d161d668e4c76a7171ca90ea71ecd3788ba
AlfinurFitraWijayaR <alfinbageur9@gmail.com>
2023-12-31 10:13:29 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const addToCartButton = document.getElementById("addToCart");
+    
+    addToCartButton.addEventListener("click", function () {
+        alert("Produk telah ditambahkan ke keranjang belanja.");
+    });
+});
